82d26d085f1e28fb3fc00c701496ba35400bfade
mekamohana <mekamohana6@gmail.com>
2025-07-02 03:40:09 -0700

    Add files via upload

---
+++ city.py
@@ -0,0 +1,13 @@
+city=input("enter a city")
+if city =="agra":
+     print("tajmahal")
+elif city =="hyderabad":
+     print("charminar")
+elif city =="delhi":
+     print("redfort")
+elif city  =="mumbai":
+     print("gateway of india")
+elif city =="chennai":
+     print("marina beech")
+else:
+     print("wrong city selected")
