65683ec7e1d32fca3568b23822b3b9d4617e3a26
mekamohana <mekamohana6@gmail.com>
2025-07-02 03:50:39 -0700

    Add files via upload

---
+++ id,pw.py
@@ -0,0 +1,6 @@
+userid=int(input("enter your userid"))
+password=int(input("enter your password"))
+if userid=="mohana"and password=="durga@25":
+  print("welcome to our net banking of canara bank")
+else:
+  print("thhe userid ag password is incorrect try again")
