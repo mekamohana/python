40d19a34e54f1c292009812230ecf81e4a3b5151
mekamohana <mekamohana6@gmail.com>
2025-07-07 03:26:57 -0700

    Add files via upload

---
+++ turtle3.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.speed(3)
+turtle.bgcolor('white')
+turtle.pensize(2)
+turtle.write('HAPPIEST BIRTHDAY SONY ') 
+for i in range(400):
+    turtle.right(1)
+    turtle.forward(1)
+turtle.color('red')
+turtle.goto()  
+turtle.done()
